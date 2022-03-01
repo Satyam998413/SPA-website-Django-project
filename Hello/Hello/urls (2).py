5add683cada933b75928d186977beb32074bec94
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-03-01 17:27:22 +0530

    somethins add
    somecodes include

--- Hello/Hello/urls (2).py
+++ Hello/Hello/urls (2).py
@@ -18,10 +18,8 @@ from django.urls import path, include
 
 admin.site.site_header = "Harry Ice Cream Admin"
 admin.site.site_title = "Harry Ice Cream Admin Portal"
-admin.site.index_title = "Welcome to Harry Ice Creams"
-
+admin.site.index_title = "Welcome to Harry Ice Cr"
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
-
+    path('', include('home.ur
 ]
